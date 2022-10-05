f5d4481a26bbada4ea0247519e1dfc874a2b01ef
jake5253 <jake.admin@gmail.com>
2022-10-05 21:23:03 +0000

    strip kernels other than 5.14 out of manifest

--- less_kernels.sh
+++ less_kernels.sh
@@ -5,6 +5,6 @@ COUNT=$(wc -l .repo/manifests/snapshot.xml | cut -d' ' -f1)
 KEEP=$(grep 'src/third_party/kernel/v5.15' .repo/manifests/snapshot.xml)
 for l in $(grep -n 'src/third_party/kernel' .repo/manifests/snapshot.xml | cut -d: -f1); do LAST=$l; done
 head -n $(( $FIRST - 1 )) .repo/manifests/snapshot.xml > /tmp/mani.fest
-echo $KEEP >> /tmp/mani.fest
+echo "$KEEP" >> /tmp/mani.fest
 LINESAFTER=$(( $COUNT - $LAST ))
-tail -n $(( $LINESAFTER + 1 )) .repo/manifests/snapshot.xml >> /tmp/mani.fest
+tail -n $(( $LINESAFTER - 2 )) .repo/manifests/snapshot.xml >> /tmp/mani.fest
