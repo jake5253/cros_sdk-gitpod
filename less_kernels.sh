ad2a41aa504c0c3c04d87645c755497ade0b7b7b
jake5253 <jake.admin@gmail.com>
2022-10-05 20:42:09 +0000

    less kernels holy shiiii

---
+++ less_kernels.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FIRST=$(grep -n -m1 'src/third_party/kernel' .repo/manifests/snapshot.xml | cut -d: -f1)
+COUNT=$(wc -l .repo/manifests/snapshot.xml | cut -d' ' -f1)
+KEEP=$(grep 'src/third_party/kernel/v5.15' .repo/manifests/snapshot.xml)
+for l in $(grep -n 'src/third_party/kernel' .repo/manifests/snapshot.xml | cut -d: -f1); do LAST=$l; done
+head -n $(( $FIRST - 1 )) .repo/manifests/snapshot.xml > /tmp/mani.fest
+echo $KEEP >> /tmp/mani.fest
+LINESAFTER=$(( $COUNT - $LAST ))
+tail -n $LINESAFTER .repo/manifests/snapshot.xml >> /tmp/mani.fest
